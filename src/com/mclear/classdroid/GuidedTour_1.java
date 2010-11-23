58a2b357af589df7fac73e897d3873c2418290a5
Kumar Bibek <kbibek@crri.co.in>
2010-11-24 01:13:13 +0530

    Guided Tour WIP

---
+++ src/com/mclear/classdroid/GuidedTour_1.java
@@ -0,0 +1,15 @@
+
+package com.mclear.classdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuidedTour_1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guided_tour_1);
+    }
+
+}
---
+++ src/com/mclear/classdroid/GuidedTour_2.java
@@ -0,0 +1,15 @@
+
+package com.mclear.classdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuidedTour_2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guided_tour_2);
+    }
+
+}
