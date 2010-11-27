adf374ec3463d6199e6bd742314380a6415ace9e
Kumar Bibek <kbibek@crri.co.in>
2010-11-27 23:33:05 +0530

    Default config activity actions

--- src/com/mclear/classdroid/GuidedTour_2.java
+++ src/com/mclear/classdroid/GuidedTour_2.java
@@ -3,6 +3,7 @@ package com.mclear.classdroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,6 +24,17 @@ public class GuidedTour_2 extends Activity {
                 Intent intent = new Intent(getBaseContext(), DefaultConfigurationActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        
+        Button btnOptions = (Button)findViewById(R.id.btnOptions);
+        btnOptions.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://crm.primaryt.co.uk/indexpb.php?registerpb"));
+                startActivity(intent);
             }
         });
     }
