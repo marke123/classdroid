3719fcddc8ea8866520368c1fc7b5626007a4383
Kumar Bibek <kbibek@crri.co.in>
2011-01-03 21:10:28 +0530

    Notification for post uploaded

--- src/com/mclear/classdroid/threads/UploaderThread.java
+++ src/com/mclear/classdroid/threads/UploaderThread.java
@@ -33,31 +33,34 @@ public class UploaderThread extends Thread {
             NotificationManager nManager = (NotificationManager) context
                     .getSystemService(Context.NOTIFICATION_SERVICE);
             nManager.cancel((int) (R.string.app_name + postId));
+            nManager.cancel(R.string.app_name);
 
             dbAdapter.open();
             Post post = dbAdapter.getPostById(postId);
-            Pupil pupil = dbAdapter.getPupilById(post.getPupilId());
-            PupilServices service = dbAdapter.getPupilServiceByPupilId(post.getPupilId(),
-                    PupilServices.TYPE_PRIMARYBLOGGER);
-            dbAdapter.close();
-            if (service.getIsEnabled() == PupilServices.SERVICE_ENABLED) {
-                dbAdapter.open();
-                String url = WebUtils.uploadPostToWordpress(pupil, post.getLocalImagePath(), ""
-                        + post.getGrade(), service, context);
-                post.setIsPosted(1);
-                post.setReturnedString(url);
-                dbAdapter.updatePost(post);
+            if (post.getIsPosted() != 1) {
+                Pupil pupil = dbAdapter.getPupilById(post.getPupilId());
+                PupilServices service = dbAdapter.getPupilServiceByPupilId(post.getPupilId(),
+                        PupilServices.TYPE_PRIMARYBLOGGER);
                 dbAdapter.close();
-                sendNotification();
-            } else {
-                post.setReturnedString("");
-            }
-            dbAdapter.open();
-            service = dbAdapter.getPupilServiceByPupilId(post.getPupilId(),
-                    PupilServices.TYPE_XPARENA);
-            dbAdapter.close();
-            if (service.getIsEnabled() == PupilServices.SERVICE_ENABLED) {
-                WebUtils.uploadPostToXPArena(service, pupil, post.getReturnedString(), "500");
+                if (service.getIsEnabled() == PupilServices.SERVICE_ENABLED) {
+                    dbAdapter.open();
+                    String url = WebUtils.uploadPostToWordpress(pupil, post.getLocalImagePath(), ""
+                            + post.getGrade(), service, context);
+                    post.setIsPosted(1);
+                    post.setReturnedString(url);
+                    dbAdapter.updatePost(post);
+                    dbAdapter.close();
+                    sendNotification();
+                } else {
+                    post.setReturnedString("");
+                }
+                dbAdapter.open();
+                service = dbAdapter.getPupilServiceByPupilId(post.getPupilId(),
+                        PupilServices.TYPE_XPARENA);
+                dbAdapter.close();
+                if (service.getIsEnabled() == PupilServices.SERVICE_ENABLED) {
+                    WebUtils.uploadPostToXPArena(service, pupil, post.getReturnedString(), "500");
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -82,8 +85,15 @@ public class UploaderThread extends Thread {
         NotificationManager nManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
         nManager.cancel(R.string.app_name);
+
+        Intent intent = new Intent(context, ClassdroidService.class);
+        intent.putExtra("id", postId);
+        PendingIntent pendingIntent = PendingIntent.getService(context, (int) postId, intent, 0);
         Notification notification = new Notification(R.drawable.icon, context
                 .getString(R.string.lab_post_uploaded), System.currentTimeMillis());
+        notification.setLatestEventInfo(context, context.getString(R.string.lab_post_uploaded),
+                context.getString(R.string.lab_assignment_uploaded), pendingIntent);
+
         nManager.notify(R.string.app_name, notification);
     }
 }
